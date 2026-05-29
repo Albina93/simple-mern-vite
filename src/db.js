fa08d117378103238e70262e13d3055941afc1b6
Albina93 <alba.tleubergen@gmail.com>
2026-05-29 00:58:43 -0400

    Update env variable name

--- src/db.js
+++ src/db.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.DATABASE_URI);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = mongoose;
